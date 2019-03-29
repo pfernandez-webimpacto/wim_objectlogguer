84a11bcf68cee756176e7b8a477ebdc68221266e
Pablo <pfernandez@webimpacto.es>
2019-03-29 08:58:57 +0100

    WEB-1669 Identación

--- wim_objectlogguer.php
+++ wim_objectlogguer.php
@@ -32,76 +32,76 @@ class Wim_objectlogguer extends Module
 {
     public function __construct()
   {
-    $this->name = 'wim_objectlogguer';
-    $this->tab = 'front_office_features';
-    $this->version = '1.0.0';
-    $this->author = 'Pablo';
-    $this->need_instance = 0;
-    $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_); 
-    $this->bootstrap = true;
- 
-    parent::__construct();
- 
-    $this->displayName = $this->l('wim_objectlogguer');
-    $this->description = $this->l('Description of my module.');
- 
-    $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+          $this->name = 'wim_objectlogguer';
+          $this->tab = 'front_office_features';
+          $this->version = '1.0.0';
+          $this->author = 'Pablo';
+          $this->need_instance = 0;
+          $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_); 
+          $this->bootstrap = true;
+          
+          parent::__construct();
+          
+          $this->displayName = $this->l('wim_objectlogguer');
+          $this->description = $this->l('Description of my module.');
+          
+          $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
 
   }
 
     public function install()
     {    
-       include(dirname(__FILE__).'\sql\install.php');
-       // return parent::install();
+          include(dirname(__FILE__).'\sql\install.php');
+          // return parent::install();
 
-       return parent::install() &&
-       $this->registerHook('actionObjectAddAfter') &&
-       $this->registerHook('actionObjectDeleteAfter') &&
-       $this->registerHook('actionObjectUpdateAfter');
+          return parent::install() &&
+          $this->registerHook('actionObjectAddAfter') &&
+          $this->registerHook('actionObjectDeleteAfter') &&
+          $this->registerHook('actionObjectUpdateAfter');
     }
 
 
      public function hookActionObjectUpdateAfter($params)
     {
 
-      $anadir = new ObjectLogger();
-      $anadir->affected_object = $params['object']->id;
-      $anadir->action_type = 'Update';
-      $anadir->object_type = get_class($params['object']);
-      $anadir->message = "Object ". get_class($params['object']) . " with id " . $params['object']->id . " update";
-      $anadir->date_add = date("Y-m-d H:i:s");
-      if(get_class($params['object']) != "ObjectLogger"){
-      $anadir->add();
-      }
+          $anadir = new ObjectLogger();
+          $anadir->affected_object = $params['object']->id;
+          $anadir->action_type = 'Update';
+          $anadir->object_type = get_class($params['object']);
+          $anadir->message = "Object ". get_class($params['object']) . " with id " . $params['object']->id . " update";
+          $anadir->date_add = date("Y-m-d H:i:s");
+          if(get_class($params['object']) != "ObjectLogger"){
+          $anadir->add();
+          }
     }
 
 
     public function hookActionObjectAddAfter($params)
     {
 
-      $after = new ObjectLogger();
-      $after->affected_object = $params['object']->id;
-      $after->action_type = 'Add';
-      $after->object_type = get_class($params['object']);
-      $after->message = "Object ". get_class($params['object']) . " with id " . $params['object']->id . " add";
-      $after->date_add = date("Y-m-d H:i:s");
-      if(get_class($params['object']) != "ObjectLogger"){
-      $after->add();
-      } 
+          $after = new ObjectLogger();
+          $after->affected_object = $params['object']->id;
+          $after->action_type = 'Add';
+          $after->object_type = get_class($params['object']);
+          $after->message = "Object ". get_class($params['object']) . " with id " . $params['object']->id . " add";
+          $after->date_add = date("Y-m-d H:i:s");
+          if(get_class($params['object']) != "ObjectLogger"){
+          $after->add();
+          } 
     }
 
 
     public function hookActionObjectDeleteAfter($params)
     {
 
-      $del = new ObjectLogger();
-      $del->affected_object = $params['object']->id;
-      $del->action_type = 'Delete';
-      $del->object_type = get_class($params['object']);
-      $del->message = "Object ". get_class($params['object']) . " with id " . $params['object']->id . " delete";
-      $del->date_add = date("Y-m-d H:i:s");
-      if(get_class($params['object']) != "ObjectLogger"){
-      $del->add();
-      }
+          $del = new ObjectLogger();
+          $del->affected_object = $params['object']->id;
+          $del->action_type = 'Delete';
+          $del->object_type = get_class($params['object']);
+          $del->message = "Object ". get_class($params['object']) . " with id " . $params['object']->id . " delete";
+          $del->date_add = date("Y-m-d H:i:s");
+          if(get_class($params['object']) != "ObjectLogger"){
+          $del->add();
+          }
     }
 }
